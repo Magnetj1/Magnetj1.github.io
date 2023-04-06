8055c496d0f30429c6517f957c6dcb18f713688e
Magne Tjärnlund <matj1116@storsjogymnasiet.se>
2023-04-06 10:51:55 +0200

    basicly done

---
+++ gissa/gissa.js
@@ -0,0 +1,17 @@
+const slumptal = math.floor(math.random()*9)+1;
+let antalgissningar = 0;
+
+
+
+console.log("slumptalet:"+ slumptal)
+
+function gissaTal(){
+console.log("click")
+let textfelt_gissa = document.getElementById("gissa").value;
+let gissa_int = Number(textfelt_gissa);
+let vinn = false
+let p_svar = document.getElementById("p_svar")
+console.log("textfelt=" + textfelt_gissa)
+
+}
+
