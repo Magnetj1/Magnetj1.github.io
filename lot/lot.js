f3b71ca9bcf8f5e3c46c6957fad71fc563ccfad2
Magne Tjärnlund <matj1116@storsjogymnasiet.se>
2023-05-04 11:47:53 +0200

    adss

--- lot/lot.js
+++ lot/lot.js
@@ -1,7 +1,7 @@
 const svar_div = document.getElementById("svar_div")
 const antal_vinster = document.getElementById("input_antal_vinster")
 
-const vinster =["ni...","emanuel","imacummm!!"," YOU GOT kys!","gayming setup","cat stealer","A deamon!","a dead cat killer","kys","Walter white","higsenberg","jesse","A EXPLODING nuke","exploding nigger!","sebbes ass","cat ass","the most breedable pokemon","vaporion","The blackest pokemon","jelly dougnut from pokemon","gigga nigga slayer","the pussy slayer","-Chicken .explodes"]
+const vinster =["ni...","emanuel","imacummm!!"," YOU GOT kys!","gayming setup","cat stealer","A deamon!","a dead cat killer","kys","Walter white","higsenberg","jesse","A EXPLODING nuke","exploding nigger!","sebbes ass","cat ass","the most breedable pokemon","vaporion","The blackest pokemon","jelly dougnut from pokemon","gigga nigga slayer","the pussy slayer","-Chicken .explodes","i walk with a stick i walk with a gun. i shot ni.... for fun(american police)"]
 
 function slumpClick(){
     //console.log(`click${antal_vinster .value}`)
@@ -11,7 +11,7 @@ let antalv= antal_vinster.value
 let int_antalv = parseInt(antalv)
 
 for (i=0; i<int_antalv; i++){
-let slumptal = Math.floor(Math.random() *23)
+let slumptal = Math.floor(Math.random() *24)
 
 let t_vinst = vinster[slumptal]
 
